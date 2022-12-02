52bf7cb576b2fbd70164acd4079e99837d9195c5
Milad Ahmadi <69338883+ShowAppStructure@users.noreply.github.com>
2022-12-02 04:28:06 -0800

    Complete Abstract classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,36 @@
 import 'package:flutter/material.dart';
-  import 'dart:developer' as devtools show log;
+import 'dart:developer' as devtools show log;
 
-  extension Log on Object{
-    void log()=> devtools.log(toString());
-  }
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
+
 void main() {
   runApp(const MyApp());
 }
-abstract class CanRun{
-    void run ();
-    void sit (){
-      "implemented function in abstract class cannot called without use ((super key word)) in child class".log();
-    }
+
+abstract class CanRun {
+  void run();
+
+  void sit() {
+    "implemented function in abstract class cannot called without use ((super key word)) in child class"
+        .log();
+  }
 }
-abstract class CanWalk{
-    void walk ();
+
+abstract class CanWalk {
+  void walk();
 }
+
 //1- todo you can extend from one class on the other hand you can use ((with)) key word to use many class functions
-class Cat extends CanRun{
+class Cat extends CanRun {
   @override
   void run() {
     "Cat can run - sub class".log();
   }
 }
 
-class Dog with CanRun,CanWalk{
+class Dog with CanRun, CanWalk {
   @override
   void run() {
     "Dog can run - sub class".log();
@@ -41,14 +47,42 @@ class Dog with CanRun,CanWalk{
     super.sit();
     "now you can call sit method in Dog and CanRun class".log();
   }
-
 }
+
+abstract class Vehicle{
+  @mustCallSuper
+  void speed(){
+    "any vehicle has speed ".log();
+  }
+}
+
+class BMW extends Vehicle{
+  @override
+  void speed(){
+    //3- todo you can force the implementer of abstract class to call super method the abstract methods with ((@mustCallSuper)) notifier
+    super.speed();
+
+  }
+}
+
+enum LiquidType{
+  Water,Cola
+}
+abstract class Liquid{
+  final LiquidType type;
+  Liquid({required this.type});
+}
+//4- todo you can use ((constractor)) in abstract class
+class Water extends Liquid{
+  Water():super(type: LiquidType.Cola);
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    "start log".log();
     return const MaterialApp(
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -57,20 +91,24 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-    void testAbstractClass(){
-      final cat=Cat();
-      cat.run();
-      "-----------------------------".log();
-      final dog=Dog();
-      dog.sit();
-    }
+  void testAbstractClass() {
+    final cat = Cat();
+    cat.run();
+    "-----------------------------".log();
+    final dog = Dog();
+    dog.sit();
+    "-----------------------------".log();
+    final water=Water();
+    water.type.log();
+  }
 
   @override
   Widget build(BuildContext context) {
